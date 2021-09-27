f643b4f9ad722c7ce800a81b31f804887c5c1671
Carambula <antibog6@gmail.com>
2021-09-27 12:52:39 +0300

    some new constants

--- Assets/Project/Classes/Consts.cs
+++ Assets/Project/Classes/Consts.cs
@@ -2,7 +2,14 @@
     public static class Consts {
         #region GAME_CONSTANTS
 
+        public const int DEFAULT_FIELD_SIZE_Y = 9;  
+        public const int DEFAULT_FIELD_SIZE_X = 9;  
         public const int DEFAULT_NUM_OF_WALLS = 10;  
+
+        #endregion
+        #region TAGS
+
+        public const string GAME_MANAGER_TAG = "GameManager";  
 
         #endregion
     }
